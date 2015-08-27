eee2a26b23db400b7c5db4b557341dcbb26589b0
Russell <russell.s.stewart@gmail.com>
2015-08-27 12:18:37 -0700

    Initial commit.

---
+++ eval.py
@@ -0,0 +1,77 @@
+import numpy as np
+import json
+import random
+
+import apollocaffe
+from apollocaffe.layers import NumpyData, LstmUnit, Concat, InnerProduct, EuclideanLoss, Filler
+
+def evaluate_forward(net, net_config):
+    net.clear_forward()
+    length = 20
+
+    net.f(NumpyData("prev_hidden", np.zeros((1, net_config["mem_cells"]))))
+    net.f(NumpyData("prev_mem", np.zeros((1, net_config["mem_cells"]))))
+    filler = Filler("uniform", net_config["init_range"])
+    accum = np.array([0.])
+    predictions = []
+
+    value = 0.5
+    for step in range(length):
+        # We'll be updating values in place for efficient memory usage. This 
+        # will break backprop and cause warnings. Use clear_forward to suppress.
+        net.clear_forward()
+        
+        # Add 0.5 to the sum at each step
+        net.f(NumpyData("value",
+            data=np.array(value).reshape((1, 1))))
+        accum += value
+        prev_hidden = "prev_hidden"
+        prev_mem = "prev_mem"
+        net.f(Concat("lstm_concat", bottoms=[prev_hidden, "value"]))
+        net.f(LstmUnit("lstm", bottoms=["lstm_concat", prev_mem],
+            param_names=["input_value", "input_gate", "forget_gate", "output_gate"],
+            weight_filler=filler,
+            tops=["next_hidden", "next_mem"], num_cells=net_config["mem_cells"]))
+        net.f(InnerProduct("ip", 1, bottoms=["next_hidden"]))
+        predictions.append(float(net.blobs["ip"].data.flatten()[0]))
+        # set up for next prediction by copying LSTM outputs back to inputs
+        net.blobs["prev_hidden"].data_tensor.copy_from(net.blobs["next_hidden"].data_tensor)
+        net.blobs["prev_mem"].data_tensor.copy_from(net.blobs["next_mem"].data_tensor)
+
+    targets = np.cumsum([value for _ in predictions])
+    residuals = [x - y for x, y in zip(predictions, targets)]
+    return targets, predictions, residuals
+
+def eval(config):
+    eval_net = apollocaffe.ApolloNet()
+    # evaluate the net once to set up structure before loading parameters
+    net_config = config["net"]
+    evaluate_forward(eval_net, net_config)
+    eval_net.load("%s_%d.h5" % (config["logging"]["snapshot_prefix"],
+        config["solver"]["max_iter"] - 1))
+    targets, predictions, residuals = evaluate_forward(eval_net, net_config)
+    targets = ',\t'.join([('%.2f' % x) for x in targets])
+    predictions = ',\t'.join([('%.2f' % x) for x in predictions])
+    residuals = ',\t'.join([('%.2f' % x) for x in residuals])
+    print 'The target values were:'
+    print targets
+    print ''
+    print "The network predicted:"
+    print predictions
+    print ''
+    print "The residuals are:"
+    print residuals
+
+def main():
+    parser = apollocaffe.base_parser()
+    parser.add_argument("--config", required=True)
+    args = parser.parse_args()
+    config = json.load(open(args.config, 'r'))
+    apollocaffe.set_random_seed(config["solver"]["random_seed"])
+    apollocaffe.set_device(args.gpu)
+    apollocaffe.set_cpp_loglevel(args.loglevel)
+
+    eval(config)
+
+if __name__ == "__main__":
+    main()
---
+++ train.py
@@ -0,0 +1,92 @@
+import numpy as np
+import json
+import random
+
+import apollocaffe
+from apollocaffe.layers import (NumpyData, LstmUnit, Concat, InnerProduct,
+    EuclideanLoss, Filler)
+
+def lstm_layers(net, step, filler, net_config):
+    layer_list = []
+    if step == 0:
+        prev_hidden = "lstm_seed"
+        prev_mem = "lstm_seed"
+    else:
+        prev_hidden = "lstm_hidden%d" % (step - 1)
+        prev_mem = "lstm_mem%d" % (step - 1)
+    # Concatenate the hidden output with the next input value 
+    layer_list.append(Concat("lstm_concat%d" % step,
+        bottoms=[prev_hidden, "value%d" % step]))
+    # Run the LSTM for one more step
+    layer_list.append(LstmUnit("lstm%d" % step, net_config["mem_cells"], 
+        bottoms=["lstm_concat%d" % step, prev_mem],
+        param_names=["input_value", "input_gate", "forget_gate", "output_gate"],
+        tops=["lstm_hidden%d" % step, "lstm_mem%d" % step],
+        weight_filler=filler))
+    return layer_list
+
+def forward(net, net_config):
+    net.clear_forward()
+    length = random.randrange(net_config["min_len"], net_config["max_len"])
+
+    # initialize all weights in [-0.1, 0.1]
+    filler = Filler("uniform", net_config["init_range"])
+    # initialize the LSTM memory with all 0's
+    net.f(NumpyData("lstm_seed",
+        np.zeros((net_config["batch_size"], net_config["mem_cells"]))))
+    accum = np.zeros((net_config["batch_size"],))
+
+    # Begin recurrence through 5 - 15 inputs
+    for step in range(length):
+        # Generate random inputs
+        value = np.array([random.random() for _ in range(net_config["batch_size"])])
+        # Set data of value blob to contain a batch of random numbers
+        net.f(NumpyData("value%d" % step,
+            value.reshape((-1, 1))))
+        accum += value
+        for l in lstm_layers(net, step, filler, net_config):
+            net.f(l)
+
+    # Add a fully connected layer with a bottom blob set to be the last used LSTM cell
+    # Note that the network structure is now a function of the data
+    net.f(InnerProduct("ip", 1, bottoms=["lstm_hidden%d" % (length - 1)],
+        weight_filler=filler))
+    # Add a label for the sum of the inputs
+    net.f(NumpyData("label", np.reshape(accum, (-1, 1))))
+    # Compute the Euclidean loss between the preiction and label, used for backprop
+    net.f(EuclideanLoss("euclidean", bottoms=["ip", "label"]))
+
+def train(config):
+    net = apollocaffe.ApolloNet()
+
+    net_config = config["net"]
+    solver = config["solver"]
+    logging = config["logging"]
+    loggers = [
+        apollocaffe.loggers.TrainLogger(logging["display_interval"]),
+        apollocaffe.loggers.SnapshotLogger(logging["snapshot_interval"], logging["snapshot_prefix"]),
+        ]
+    train_loss = []
+    for i in range(solver["max_iter"]):
+        forward(net, net_config)
+        train_loss.append(net.loss)
+        net.backward()
+        lr = (solver["base_lr"] * (solver["gamma"])**(i // solver["stepsize"]))
+        net.update(lr=lr, momentum=solver["momentum"],
+            clip_gradients=solver["clip_gradients"])
+        for logger in loggers:
+            logger.log(i, {"train_loss": train_loss, "apollo_net": net})
+
+def main():
+    parser = apollocaffe.base_parser()
+    parser.add_argument("--config", required=True)
+    args = parser.parse_args()
+    config = json.load(open(args.config, 'r'))
+    apollocaffe.set_random_seed(config["solver"]["random_seed"])
+    apollocaffe.set_device(args.gpu)
+    apollocaffe.set_cpp_loglevel(args.loglevel)
+
+    train(config)
+
+if __name__ == "__main__":
+    main()
